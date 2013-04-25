a81f79ebb513453bc31d3b030c1adb769c042541
William Hsu <william@tpemozilla.(none)>
2013-04-25 19:29:52 +0800

    *Gaia UI test issue:      https://github.com/mozilla/gaia-ui-tests/issues/701
        *Changes
         Initial the keyboard test of change language.
         This script did the following steps.
          1. Navigate to "Settings" page
          2. Scroll to "Keyboard" tag then click it.
          3. Check/Change language to "Spanish"
            3a. Adding judgment of check box
          4. Navigate to "Email" app
          5. Tap name/text field.
          6. After UI displayed keyboard, pressing "Global" key to change language.
          7. Verify/find a unique key on Spanish keyboard.

---
+++ gaiatest/tests/settings/test_settings_change_keyboard_language.py
@@ -0,0 +1,66 @@
+# -*- coding: UTF-8 -*-
+# This is Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from gaiatest import GaiaTestCase
+
+
+class TestChangeKeyboardLanguage(GaiaTestCase):
+
+    # Language settings locators
+    _keyboard_settings_locator = ("id", "menuItem-keyboard")
+    _select_language_locator   = ("css selector", "#keyboard ul:last-child li:nth-child(3)")
+    _checkbox_locator          = ("css selector", "#keyboard ul:last-child li input[name='keyboard.layouts.spanish']")
+    _select_text_field_locator = ("css selector", "input[type='text']")
+    _select_keyb_frame_locator = ("css selector", "#keyboard-frame iframe")
+    _language_key_locator      = ("css selector", ".keyboard-row button[data-keycode='-3']")
+    _special_key_locator       = ("css selector", ".keyboard-row button[data-keycode='241']")
+    _specific_key              = u'\xf1'
+
+    def setUp(self):
+        GaiaTestCase.setUp(self)
+
+        # Launch the Settings app
+        self.app = self.apps.launch('Settings')
+
+    def test_change_keyboard_language_settings(self):
+
+        # Navigate to keyboard settings
+        self.wait_for_element_present(*self._keyboard_settings_locator)
+        keyboard_setting = self.marionette.find_element(*self._keyboard_settings_locator)
+
+        # Select keyboard setting
+        self.marionette.tap(keyboard_setting)
+
+        # Select keyboard language
+        self.wait_for_element_present(*self._select_language_locator)
+        selected_language = self.marionette.find_element(*self._select_language_locator)
+        self.wait_for_element_present(*self._checkbox_locator)
+        checkbox = self.marionette.find_element(*self._checkbox_locator)
+        if checkbox.is_selected() == False:
+            # Temporary use single_tap() since tap() and click() cannot work here
+            selected_language.single_tap()
+
+        # --Verify the keyboard layout
+        # --launch the email app (follow manyally test case)
+        self.app = self.apps.launch('email')
+
+        # Select name field
+        self.wait_for_element_present(*self._select_text_field_locator)
+        select_text_field = self.marionette.find_element(*self._select_text_field_locator)
+        # tap() cannot work here
+        select_text_field.click()
+
+        # Switch to keyboard frame and change language -> Temporary solution. Waiting for keyboard class
+        self.marionette.switch_to_frame()
+        keybframe = self.marionette.find_element(*self._select_keyb_frame_locator)
+        self.marionette.switch_to_frame(keybframe, focus=False)
+
+        # Temporary solution since action-chain function not yet merged
+        language_key = self.marionette.find_element(*self._language_key_locator)
+        self.marionette.tap(language_key)
+        special_key = self.marionette.find_element(*self._special_key_locator).text
+
+        # Checking if exists the special key - "ñ"
+        self.assertEqual(special_key, self._specific_key)
